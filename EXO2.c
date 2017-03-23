4e1ab09cb7b36928fa98699dbce9e2d5caca257d
rBOISSON <romain.nicolas.boisson@gmail.com>
2017-03-23 15:13:16 +0100

    Add files via upload

--- EXO2.c
+++ EXO2.c
@@ -1,59 +1,65 @@
 #include "EXO2.h"
 
-
 // Tableau dynamique
 // 2.1
-	void vecset_create(struct vecset *self){
-		self -> data = malloc(sizeof(int));
-		self -> capacity = 0;
-		self -> size = 0;
-	}
+void vecset_create(struct vecset *self){
+	self -> data = malloc(sizeof(int));
+	self -> capacity = 0;
+	self -> size = 0;
+}
 	
 // 2.2 
-	void vecset_destroy(struct vecset *self){
-		free(self->data);
-	}
+void vecset_destroy(struct vecset *self){
+	free(self->data);
+}
 
 // 2.3 
-	void vecset_add(struct vecset *self, struct vec p){
-		if (self->size == self->capacity) {
-			if (self->capacity ==0){
-				self->capacity =1;
-			}
-			self->capacity *= 2;
-			struct vec *data = calloc(self->capacity, sizeof(struct vec));
-			memcpy(data, self->data, self->size * sizeof(struct vec));
-			free(self->data);
-			self->data = data;
-			free(data);
+void vecset_add(struct vecset *self, struct vec p){
+	if (self->size == self->capacity) {
+		if (self->capacity ==0){
+			self->capacity =1;
 		}
-		self->data[self->size] = p;
-		self->size += 1;	
+		self->capacity *= 2;
+		struct vec *data = calloc(self->capacity, sizeof(struct vec));
+		memcpy(data, self->data, self->size * sizeof(struct vec));
+		free(self->data);
+		self->data = data;
 	}
+	self->data[self->size] = p;
+	self->size += 1;
+	
+	printf("%zu",self->size);
+}
+	
+
 	
 // 2.4 
-	const struct vec *vecset_max(const struct vecset *self, comp_func_t func, const void *ctx){
-	struct vec max = self->data[0];
-	for (size_t i = 0 ; i < self->size ; ++i){
-		//accès a donnée dans le tableau self->data[i].x
-		if(func(&max,&(self->data[i]),NULL) < 0){
-			max=self->data[i];
+const struct vec *vecset_max(const struct vecset *self, comp_func_t func, const void *ctx){
+	if(ctx==NULL){
+		struct vec max = self->data[0];
+		for (size_t i = 0 ; i < self->size ; ++i){
+			//accès a donnée dans le tableau self->data[i].x
+			if(func(&max,&(self->data[i]),NULL) < 0){
+				max=self->data[i];
+			}
 		}
+		const struct vec *res = &max ;
+		return res;
 	}
-	const struct vec *res = &max ;
-	return res;
 }
 
 // 2.5
 const struct vec *vecset_min(const struct vecset *self, comp_func_t func, const void *ctx){
-	struct vec min = self->data[0];
-	for (size_t i = 0 ; i < self->size ; ++i){
-		if(func(&min,&(self->data[i]),NULL) > 0){
-			min=self->data[i];
+	if(ctx==NULL){
+		struct vec min = self->data[0];
+		for (size_t i = 0 ; i < self->size ; ++i){
+			if(func(&min,&(self->data[i]),NULL) > 0){
+				min=self->data[i];
+			}
 		}
-	}
-	const struct vec *res = &min;
+	const struct vec *res = &min ;
 	return res;
+	}
 }
 
 // quick sort
@@ -81,7 +87,6 @@ long vecset_partition(struct vecset *self, long i, long j, comp_func_t func) {
 	vecset_swap(self, l, j);
 	return l;
 }
-
 void vecset_quick_sort_partial(struct vecset *data, long i, long j, comp_func_t func) {
 	if (i < j) {
 		long p = vecset_partition(data, i, j,func);
@@ -92,25 +97,26 @@ void vecset_quick_sort_partial(struct vecset *data, long i, long j, comp_func_t 
 
 
 void vector_set_sort(struct vecset *self,comp_func_t func, const void *ctx) {
-	long n = self->size;
-	vecset_quick_sort_partial(self, 0, n - 1,func);
+	if(ctx ==NULL){
+		long n = self->size;
+		vecset_quick_sort_partial(self, 0, n - 1,func);
+	}
+}
+
+void vecset_push(struct vecset *self,struct vec p){
+	vecset_add(self,p);
+}
+void vecset_pop(struct vecset *self){
+	self->size -= 1;
 }
 
 
-// fonction de comparaison : 
-
-int comparaison_jarvis(const struct vec *p1,const struct vec *p2, const void *ctx){
- 		if (p1->x == p2->x){
- 			return 0;
- 		}else{
- 			if(p1->x > p2->x){
- 				return 1;
- 			}
- 			return -1;
- 		}
+const struct vec *vecset_top(const struct vecset *self){
+	const struct vec *res = &self->data[self->size-1];
+	return res;
 }
-
-
-
-
+const struct vec *vecset_second(const struct vecset *self){
+	const struct vec *res = &self->data[self->size-2];
+	return res;
+}
 
--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 CFLAG=-c -Wall -std=c99 -g -o
 
-
 all: hull clean
-hull: geo_struc.o hull.o EXO2.o marche_jarvis.o
+hull: geo_struc.o EXO2.o graham.o hull.o 
 	gcc -Wall -std=c99 -g -o hull $^
 
-hull.o: hull.c geo_struc.h EXO2.h
+hull.o: hull.c geo_struc.h EXO2.h graham.h
 	gcc $(CFLAG) hull.o $<
 	
 geo_struc.o: geo_struc.c geo_struc.h
@@ -14,8 +13,8 @@ geo_struc.o: geo_struc.c geo_struc.h
 EXO2.o: EXO2.c EXO2.h geo_struc.h
 	gcc $(CFLAG) EXO2.o EXO2.c
 
-marche_jarvis.o: marche_jarvis.c marche_jarvis.h EXO2.h
-	gcc $(CFLAG) marche_jarvis.o marche_jarvis.c 
+graham.o: graham.c graham.h EXO2.h
+	gcc $(CFLAG) graham.o graham.c
 	
 clean:
 	rm -f *.o
