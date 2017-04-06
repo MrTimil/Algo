33d94d789c70517a43f9109b67c527a77639c1b8
MrTimil <timil25@hotmail.fr>
2017-04-06 16:30:32 +0200

    Add files via upload

---
+++ bibli.c
@@ -0,0 +1,78 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "navy.h"
+#define BUFSIZE 256
+
+
+// Structure 
+// dans le main struct state tab1[10][10];
+// la struct state permet de décrire une case 
+struct state{
+	int ligne;
+	int colonne;
+	int nbTir; 	
+	bool bateauIsHere;
+	bool mineIsHere;
+	
+};
+// permet de décrire le bateau
+struct bateau{
+	int ligne;
+	int colonne;
+	int size;
+	int direction;
+	int nbToucher;
+};
+
+
+// Fonctions 
+
+
+// création des bateaux 
+
+/* flottes :	1 bateau de 5
+				1 bateau de 4
+				2 bateau de 3
+				1 bateau de 2 
+				
+*/
+
+
+
+/* 	
+	fonction qui choisi une case au hasard
+	on test sur un peu insérer le 1 er bateau dans un direction choisie aléatoirement (entre 1 et 4)
+	si oui alors on l'insere dans notre terrain, sinon on essai une autre direction, quand on a usé les 4 directions possible, on retire une case différente aléatoirement
+*/
+void add_ship(struct state *tab1){
+	
+}
+	
+/*
+	On choisi de placer les mines (pas aléatoire, établir une strats) et on les places dans le terrain enemis pour ne pas tirer sur nos propre mines car on perdrait un tir et on détruirait notre propre mine
+	
+*/
+void add_mine(struct state *tab1){
+
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- navy.h
+++ navy.h
@@ -11,25 +11,6 @@
 #define TONY NULL
 
 
-// Structure 
-
-// la struct state permet de décrire une case 
-struct state { 
-	int ligne;
-	int colonne;
-	int nbTir; 	
-	bool bateauIsHere;
-	bool mineIsHere;
-	
-};
-// permet de décrire le bateau
-struct bateau {
-	int ligne;
-	int colonne;
-	int size;
-	int direction;
-	int nbToucher;
-};
 
  
 // fonctions 
